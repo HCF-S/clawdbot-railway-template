9d0e5d303a22e8130b81201dc826174f85484522
William <arenabpo@gmail.com>
2026-02-03 18:13:57 +0800

    change to use 3000 port

--- src/server.js
+++ src/server.js
@@ -14,7 +14,7 @@ import { createSetupRouter } from "./routes/setup/index.js";
 // Prefer OPENCLAW_PUBLIC_PORT (set in the Dockerfile / template) over PORT.
 // Keep CLAWDBOT_PUBLIC_PORT as a backward-compat alias for older templates.
 const PORT = Number.parseInt(
-  process.env.OPENCLAW_PUBLIC_PORT ?? process.env.CLAWDBOT_PUBLIC_PORT ?? process.env.PORT ?? "8080",
+  process.env.OPENCLAW_PUBLIC_PORT ?? process.env.CLAWDBOT_PUBLIC_PORT ?? process.env.PORT ?? "3000",
   10,
 );
 
